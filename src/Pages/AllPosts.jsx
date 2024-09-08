15fc1636e3fdf0ea6a563dcb43f3772a33a68038
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2024-09-08 18:46:52 +0500

    Commit

--- src/Pages/AllPosts.jsx
+++ src/Pages/AllPosts.jsx
@@ -3,161 +3,218 @@ import Chips from "../Components/chips";
 import { useState, useEffect } from "react";
 function AllPosts() {
 
-    const [chosenCategory, setChosenCategory] = useState("Muharram");
-
-    const [monthName, setMonthName] = useState([
-        "Muharram",
-        "Safar",
-        "Rabi al-Awwal",
-        "Rabi al-Thani",
-        "Jumada al-Awwal",
-        "Jumada al-Thani",
-        "Rajab",
-        "Sha’ban",
-        "Ramadan",
-        "Shawwal",
-        "Dhu al-Qi’dah",
-        "Dhu al-Hijjah",
-    ]);
+  const [chosenCategory, setChosenCategory] = useState("Muharram");
+  let [posts , SetPosts] = useState([]);
 
 
-    const apiKey = "d3lHSzlwMm5oWlhiZ3RuM1hkTWZzbm1SNWRzMTdEV3k4d085R2YzUw==";
-    const [allCountries, setAllCountries] = useState([]);
-    const [selectedCountry, setSelectedCountry] = useState("ALL_COUNTRIES"); // Default to "All Countries"
-    const [allCities, setAllCities] = useState([]);
-    const [selectedCity, setSelectedCity] = useState("ALL_CITIES"); // Default to "All Cities"
-  
-    // Fetch countries when the component mounts
-    useEffect(() => {
-      const fetchCountries = async () => {
+
+
+  const [monthName, setMonthName] = useState([
+    "Muharram",
+    "Safar",
+    "Rabi al-Awwal",
+    "Rabi al-Thani",
+    "Jumada al-Awwal",
+    "Jumada al-Thani",
+    "Rajab",
+    "Sha’ban",
+    "Ramadan",
+    "Shawwal",
+    "Dhu al-Qi’dah",
+    "Dhu al-Hijjah",
+  ]);
+
+
+  const apiKey = "d3lHSzlwMm5oWlhiZ3RuM1hkTWZzbm1SNWRzMTdEV3k4d085R2YzUw==";
+  const [allCountries, setAllCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("ALL_COUNTRIES"); // Default to "All Countries"
+  const [allCities, setAllCities] = useState([]);
+  const [selectedCity, setSelectedCity] = useState("ALL_CITIES"); // Default to "All Cities"
+
+  // Fetch countries when the component mounts
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const headers = new Headers();
+      headers.append("X-CSCAPI-KEY", apiKey);
+
+      const requestOptions = {
+        method: "GET",
+        headers: headers,
+        redirect: "follow",
+      };
+
+      try {
+        const response = await fetch(
+          "https://api.countrystatecity.in/v1/countries",
+          requestOptions
+        );
+        const result = await response.json();
+        setAllCountries(result);
+      } catch (error) {
+        console.error("Error fetching countries:", error);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+
+  // Fetch cities based on the selected country
+  useEffect(() => {
+    if (selectedCountry && selectedCountry !== "ALL_COUNTRIES") {
+      const fetchCity = async () => {
         const headers = new Headers();
         headers.append("X-CSCAPI-KEY", apiKey);
-  
+
         const requestOptions = {
           method: "GET",
           headers: headers,
           redirect: "follow",
         };
-  
+
         try {
           const response = await fetch(
-            "https://api.countrystatecity.in/v1/countries",
+            `https://api.countrystatecity.in/v1/countries/${selectedCountry}/cities`,
             requestOptions
           );
           const result = await response.json();
-          setAllCountries(result);
+          setAllCities(result);
         } catch (error) {
-          console.error("Error fetching countries:", error);
+          console.error("Error fetching cities:", error);
         }
       };
-  
-      fetchCountries();
-    }, []);
-  
-    // Fetch cities based on the selected country
-    useEffect(() => {
-      if (selectedCountry && selectedCountry !== "ALL_COUNTRIES") {
-        const fetchCity = async () => {
-          const headers = new Headers();
-          headers.append("X-CSCAPI-KEY", apiKey);
-  
-          const requestOptions = {
-            method: "GET",
-            headers: headers,
-            redirect: "follow",
-          };
-  
-          try {
-            const response = await fetch(
-              `https://api.countrystatecity.in/v1/countries/${selectedCountry}/cities`,
-              requestOptions
-            );
-            const result = await response.json();
-            setAllCities(result);
-          } catch (error) {
-            console.error("Error fetching cities:", error);
-          }
-        };
-  
-        fetchCity();
-      } else {
-        // Reset cities to "All Cities" if "All Countries" is selected
-        setAllCities([]);
-        setSelectedCity("ALL_CITIES");
+
+      fetchCity();
+    } else {
+      // Reset cities to "All Cities" if "All Countries" is selected
+      setAllCities([]);
+      setSelectedCity("ALL_CITIES");
+    }
+  }, [selectedCountry]);
+
+  const handleCountryChange = (event) => {
+    const newCountry = event.target.value;
+    setSelectedCountry(newCountry);
+    setSelectedCity("ALL_CITIES"); // Reset city to default when country changes
+  };
+
+  const handleCityChange = (event) => {
+    setSelectedCity(event.target.value);
+  };
+
+
+  useEffect(() => {
+      let fetchData = () => {
+          fetch("https://sarfonahwkidunya.el.r.appspot.com/api/aaraas/")
+          .then((response) => response.json())
+          .then((data) => SetPosts(data.data.aaraasList))
       }
-    }, [selectedCountry]);
+
+      fetchData()
+  } ,[])
+
+
+ console.log(posts);
+ 
   
-    const handleCountryChange = (event) => {
-      const newCountry = event.target.value;
-      setSelectedCountry(newCountry);
-      setSelectedCity("ALL_CITIES"); // Reset city to default when country changes
-    };
-  
-    const handleCityChange = (event) => {
-      setSelectedCity(event.target.value);
-    };
 
-    return (
-        <>
+  return (
+    <>
 
-            {/* chips started  */}
-            <div className="flex justify-center items-center flex-wrap rounded-xl shadow-lg mb-5">
-                {monthName.map((month, index) => (
-                    <Chips
-                        key={index}
-                        title={month}
-                        onclick={() => setChosenCategory(month)}
-                        isSelected={month === chosenCategory}
-                    />
-                ))}
-            </div>
-
-
-            {/* chips ended  */}
-
-
-
-            <div className="flex justify-center items-center flex-wrap gap-10">
-      <div className="input-group">
-        <label htmlFor="country-select">Select Country</label>
-        <select
-          id="country-select"
-          value={selectedCountry}
-          onChange={handleCountryChange}
-          className="border border-blue-500 h-[40px] px-4 outline outline-blue-200"
-        >
-          <option value="ALL_COUNTRIES">All Countries</option> {/* Default "All Countries" option */}
-          {allCountries.map((country) => (
-            <option key={country.iso2} value={country.iso2}>
-              {country.name}
-            </option>
-          ))}
-        </select>
+      {/* chips started  */}
+      <div className="flex justify-center items-center flex-wrap rounded-xl shadow-lg mb-5">
+        {monthName.map((month, index) => (
+          <Chips
+            key={index}
+            title={month}
+            onclick={() => setChosenCategory(month)}
+            isSelected={month === chosenCategory}
+          />
+        ))}
       </div>
 
-      <div className="input-group">
-        <label htmlFor="city-select">Select City</label>
-        <select
-          id="city-select"
-          onChange={handleCityChange}
-          value={selectedCity}
-          className="border border-blue-500 h-[40px] px-4 outline outline-blue-200"
-          required
-        >
-          <option value="ALL_CITIES">All Cities</option> {/* Default "All Cities" option */}
-          {allCities.length > 0
-            ? allCities.map((city) => (
+
+      {/* chips ended  */}
+
+
+
+      <div className="flex justify-center items-center flex-wrap gap-10">
+        <div className="input-group">
+          <label htmlFor="country-select">Select Country</label>
+          <select
+            id="country-select"
+            value={selectedCountry}
+            onChange={handleCountryChange}
+            className="border border-blue-500 h-[40px] px-4 outline outline-blue-200 w-full"
+          >
+            <option value="ALL_COUNTRIES">All Countries</option> {/* Default "All Countries" option */}
+            {allCountries.map((country) => (
+              <option key={country.iso2} value={country.iso2}>
+                {country.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="city-select">Select City</label>
+          <select
+            id="city-select"
+            onChange={handleCityChange}
+            value={selectedCity}
+            className="border border-blue-500 h-[40px] px-4 outline outline-blue-200 w-full"
+            required
+          >
+            <option value="ALL_CITIES">All Cities</option> {/* Default "All Cities" option */}
+            {allCities.length > 0
+              ? allCities.map((city) => (
                 <option value={city.iso2} key={city.iso2}>
                   {city.name}
                 </option>
               ))
-            : ""}
-        </select>
+              : ""}
+          </select>
+        </div>
       </div>
-    </div>
 
-        </>
-    )
+
+
+
+      <div className="p-4">
+        {/* Responsive wrapper with overflow-x-auto for horizontal scroll */}
+        <div className="overflow-x-auto">
+          <table className="min-w-full bg-white border border-gray-200">
+            <thead>
+              <tr className="bg-blue-500 text-white">
+                <th className="py-3 px-4 text-left">Name</th>
+                <th className="py-3 px-4 text-left">Father Name</th>
+                <th className="py-3 px-4 text-left">Date Of Birth</th>
+                <th className="py-3 px-4 text-left">Date Of Wissal</th>
+                <th className="py-3 px-4 text-left">Country</th>
+                <th className="py-3 px-4 text-left">City</th>
+              </tr>
+            </thead>
+            <tbody>
+            {posts.map((post, index) => (
+              <tr
+                key={index}
+                className={`border-b border-gray-200 hover:bg-gray-100 ${
+                  index % 2 === 0 ? 'bg-gray-100' : 'bg-white'
+                } w-full`}
+              >
+                <td className="py-3 px-4 whitespace-normal">{post.name}</td>
+                <td className="py-3 px-4 whitespace-normal">{post.father_name}</td>
+                <td className="py-3 px-4 whitespace-normal">{post.birth_date_islamic.date} - {post.birth_date_islamic.month}</td>
+                <td className="py-3 px-4 whitespace-normal">{post.wisaal_date_islamic.date} - {post.wisaal_date_islamic.month}</td>
+                <td className="py-3 px-4 whitespace-normal">{post.country}</td>
+                <td className="py-3 px-4 whitespace-normal">{post.city}</td>
+              </tr>
+            ))}
+          </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  )
 }
 
 
